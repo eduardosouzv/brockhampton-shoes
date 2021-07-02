ae5700da5d034e8063ea6a78b13dfa2355349ee3
Eduardo Souza <eduardosouzv@gmail.com>
2021-07-02 19:40:45 -0300

    feat: admin products manager

---
+++ view/admin/products.php
@@ -0,0 +1,127 @@
+<!DOCTYPE html>
+
+<head>
+  <meta charset="UTF-8">
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+  <link href="https://fonts.googleapis.com/css2?family=Inter:wght@300;600&display=swap" rel="stylesheet">
+
+  <link rel="stylesheet" href="https://unpkg.com/flickity@2/dist/flickity.min.css">
+
+  <link rel="icon" href="../assets/logo.png">
+
+  <link rel="stylesheet" href="../../styles/global.css">
+  <link rel="stylesheet" href="../../styles/admin/products.css">
+
+  <script src="https://unpkg.com/flickity@2/dist/flickity.pkgd.min.js"></script>
+  <title>Dashboard | BROCKHAMPTON</title>
+</head>
+
+<body>
+  <div class="page_container">
+    <div>
+      <?php include "../../components/admin/menu.php"; ?>
+    </div>
+    <main>
+      <div>
+        <h1 class="title">
+          <span>Produtos</span>
+        </h1>
+      </div>
+
+      <div class="product_list">
+        <div class="product">
+          <img src="../../assets/products/shoe.jpg" alt="tenis">
+          <p>AIR MAX 123</p>
+          <div class="actions">
+            <i class="fas fa-edit fa-lg"></i>
+            <i class="fas fa-times fa-lg"></i>
+          </div>
+        </div>
+
+        <div class="product">
+          <img src="../../assets/products/shoe.jpg" alt="tenis">
+          <p>AIR MAX 123</p>
+          <div class="actions">
+            <i class="fas fa-edit fa-lg"></i>
+            <i class="fas fa-times fa-lg"></i>
+          </div>
+        </div>
+
+        <div class="product">
+          <img src="../../assets/products/shoe.jpg" alt="tenis">
+          <p>AIR MAX 123</p>
+          <div class="actions">
+            <i class="fas fa-edit fa-lg"></i>
+            <i class="fas fa-times fa-lg"></i>
+          </div>
+        </div>
+
+        <div class="product">
+          <img src="../../assets/products/shoe.jpg" alt="tenis">
+          <p>AIR MAX 123</p>
+          <div class="actions">
+            <i class="fas fa-edit fa-lg"></i>
+            <i class="fas fa-times fa-lg"></i>
+          </div>
+        </div>
+
+        <div class="product">
+          <img src="../../assets/products/shoe.jpg" alt="tenis">
+          <p>AIR MAX 123</p>
+          <div class="actions">
+            <i class="fas fa-edit fa-lg"></i>
+            <i class="fas fa-times fa-lg"></i>
+          </div>
+        </div>
+
+        <div class="product">
+          <img src="../../assets/products/shoe.jpg" alt="tenis">
+          <p>AIR MAX 123</p>
+          <div class="actions">
+            <i class="fas fa-edit fa-lg"></i>
+            <i class="fas fa-times fa-lg"></i>
+          </div>
+        </div>
+
+        <div class="product">
+          <img src="../../assets/products/shoe.jpg" alt="tenis">
+          <p>AIR MAX 123</p>
+          <div class="actions">
+            <i class="fas fa-edit fa-lg"></i>
+            <i class="fas fa-times fa-lg"></i>
+          </div>
+        </div>
+
+        <div class="product">
+          <img src="../../assets/products/shoe.jpg" alt="tenis">
+          <p>AIR MAX 123</p>
+          <div class="actions">
+            <i class="fas fa-edit fa-lg"></i>
+            <i class="fas fa-times fa-lg"></i>
+          </div>
+        </div>
+
+        <div class="product">
+          <img src="../../assets/products/shoe.jpg" alt="tenis">
+          <p>AIR MAX 123</p>
+          <div class="actions">
+            <i class="fas fa-edit fa-lg"></i>
+            <i class="fas fa-times fa-lg"></i>
+          </div>
+        </div>
+
+        <div class="product">
+          <img src="../../assets/products/shoe.jpg" alt="tenis">
+          <p>AIR MAX 123</p>
+          <div class="actions">
+            <i class="fas fa-edit fa-lg"></i>
+            <i class="fas fa-times fa-lg"></i>
+          </div>
+        </div>
+      </div>
+    </main>
+  </div>
+</body>
+
+</html>
