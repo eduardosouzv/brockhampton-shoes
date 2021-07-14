48ddedfd77e0136390582cf7df65cbae49737d61
Eduardo Souza <eduardosouzv@gmail.com>
2021-07-14 13:27:19 -0300

    feat: findall and createproduct operations

--- api/app/model/Product.php
+++ api/app/model/Product.php
@@ -1,25 +1,45 @@
 <?php
 
-class Product
+include '../../../database/Connection.php';
+
+class Product extends Connection
 {
+  public function createProduct($product_name, $product_description, $product_price, $product_size)
+  {
+
+    $query_to_find_size_id = "SELECT * FROM sizes WHERE size_name =" . $product_size;
+    $found_size = Connection::prepare($query_to_find_size_id);
+    $found_size->execute();
+    $found_size = (array) $found_size->fetchAll()[0];
+    $product_size_id = $found_size["id"];
+
+    if (!isset($product_size_id)) {
+      http_response_code(404);
+      throw new Exception('size not found');
+    }
+
+    $query = "INSERT INTO products (product_name, description,price, sizes_id)
+    VALUES (:product_name, :product_description, :product_price, :product_size)";
+
+    $result = Connection::prepare($query);
+    $result->bindParam(':product_name', $product_name);
+    $result->bindParam(':product_description', $product_description);
+    $result->bindParam(':product_price', $product_price);
+    $result->bindParam(':product_size', $product_size_id);
+
+    $result->execute();
+
+    http_response_code(201);
+    return [
+      "product_created" =>  $product_name,
+    ];
+  }
+
   public function findAll()
   {
-    return [
-      [
-        "model" => "air max",
-        "size" => [38, 39, 40, 41],
-        "color" => "black",
-      ],
-      [
-        "model" => "yeezy boost",
-        "size" => [38, 39, 40, 41],
-        "color" => "white",
-      ],
-      [
-        "model" => "jordan",
-        "size" => [38, 39, 40, 41],
-        "color" => "red",
-      ],
-    ];
+    $sql = "SELECT * FROM products";
+    $all_products = Connection::prepare($sql);
+    $all_products->execute();
+    return $all_products->fetchAll();
   }
 }
