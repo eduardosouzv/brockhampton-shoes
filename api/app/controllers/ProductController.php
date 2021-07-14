97dd8dce3e29ac327ffe53db68d60856220f7fad
Eduardo Souza <eduardosouzv@gmail.com>
2021-07-14 13:26:38 -0300

    feat: product creation

--- api/app/controllers/ProductController.php
+++ api/app/controllers/ProductController.php
@@ -1,5 +1,5 @@
 <?php
-include '../../model/Product.php';
+require '../../model/Product.php';
 
 class ProductController
 {
@@ -7,5 +7,23 @@ class ProductController
   {
     $product = new Product();
     return $product->findAll();
+  }
+
+  function createProduct($product_name, $product_description, $product_price, $product_size)
+  {
+    try {
+      $product = new Product();
+
+      if (!isset($product_name) && !isset($product_description) && !isset($product_price) && !isset($product_size)) {
+        http_response_code(404);
+        throw new Exception('invalid data');
+      }
+
+      return $product->createProduct($product_name, $product_description, $product_price, $product_size);
+    } catch (Exception $e) {
+      return [
+        "message" =>  $e->getMessage(),
+      ];
+    }
   }
 }
