b906eee27017b1c5679961a04f5ffd45fe910d0a
Eduardo Souza <eduardosouzv@gmail.com>
2021-08-04 22:25:57 -0300

    feat: orders action

--- api/app/controllers/OrderController.php
+++ api/app/controllers/OrderController.php
@@ -24,4 +24,58 @@ class OrderController
       ];
     }
   }
+
+  function updateStatus($id, $status)
+  {
+    try {
+      $order = new Order();
+
+      if (
+        !isset($id) ||
+        !isset($status)
+      ) {
+        http_response_code(400);
+        throw new Exception('data error');
+      }
+
+      return $order->updateStatus($id, $status);
+    } catch (Exception $e) {
+      http_response_code(500);
+      return [
+        "message" =>  $e->getMessage(),
+      ];
+    }
+  }
+
+  function findOrdersByStatus($status)
+  {
+    try {
+      $order = new Order();
+
+      return $order->findOpenOrders($status);
+    } catch (Exception $e) {
+      http_response_code(500);
+      return [
+        "message" =>  $e->getMessage(),
+      ];
+    }
+  }
+  function findOrderInformationByID($id)
+  {
+    try {
+      $order = new Order();
+
+      if (!isset($id)) {
+        http_response_code(400);
+        return;
+      }
+
+      return $order->findOrderInformationByID($id);
+    } catch (Exception $e) {
+      http_response_code(500);
+      return [
+        "message" =>  $e->getMessage(),
+      ];
+    }
+  }
 }
--- api/app/model/Order.php
+++ api/app/model/Order.php
@@ -69,4 +69,102 @@ class Order extends Connection
       "order_created" =>  $last_insert_id,
     ];
   }
+  function updateStatus($id, $status)
+  {
+    $query_uptate_order_status = 'UPDATE orders SET status = :status WHERE id = :id;';
+
+    $uptate_order_status = Connection::prepare($query_uptate_order_status);
+    $uptate_order_status->bindParam(':id', $id);
+    $uptate_order_status->bindParam(':status', $status);
+    $uptate_order_status->execute();
+
+    http_response_code(201);
+
+    return [
+      "order_updated" =>  $id,
+    ];
+  }
+
+  function findOpenOrders($status)
+  {
+    if (!isset($status)) {
+      $query_select_pending_orders = 'SELECT * FROM orders;';
+
+      $select_pending_orders = Connection::prepare($query_select_pending_orders);
+      $select_pending_orders->execute();
+
+      $select_pending_orders = $select_pending_orders->fetchAll();
+
+      return $select_pending_orders;
+    }
+
+    if ($status === 'PROCESSANDO' || $status === 'CANCELADO' || $status === 'DESPACHADO') {
+      $query_select_pending_orders = 'SELECT * FROM orders WHERE status = :status;';
+
+      $select_pending_orders = Connection::prepare($query_select_pending_orders);
+      $select_pending_orders->bindParam(':status', $status);
+      $select_pending_orders->execute();
+
+      $select_pending_orders = $select_pending_orders->fetchAll();
+
+      http_response_code(200);
+      return $select_pending_orders;
+    }
+    http_response_code(400);
+    return;
+  }
+  function findOrderInformationByID($id)
+  {
+    $query_select_order_info_by_id =
+      'SELECT o.id, u.username, u.street,u.`number`,u.district,u.state,u.zip,
+        p.product_name,p.price,p.image_link, s.size_name, ohp.quantity 
+        FROM orders o 
+        INNER JOIN orders_has_products ohp 
+        ON ohp.orders_id = o.id 
+        INNER JOIN products p 
+        ON p.id = ohp.products_id 
+        INNER JOIN products_has_sizes phs 
+        ON phs.products_id = p.id 
+        INNER JOIN sizes s 
+        ON s.id = phs.sizes_id 
+        INNER JOIN users u 
+        ON u.id = o.users_id 
+        WHERE ohp.size_id = phs.sizes_id AND o.id = :id';
+
+    $select_order_info_by_id = Connection::prepare($query_select_order_info_by_id);
+    $select_order_info_by_id->bindParam(':id', $id);
+    $select_order_info_by_id->execute();
+
+    $orders_info = (array) $select_order_info_by_id->fetchAll();
+
+    if (!count($orders_info)) {
+      return [];
+    }
+
+    $products = [];
+
+    foreach ($orders_info as $product) {
+      $product = (array) $product;
+
+      array_push($products, [
+        "product_name" => $product["product_name"],
+        "price" => $product["price"],
+        "image_link" => $product["image_link"],
+        "size_name" => $product["size_name"],
+        "quantity" => $product["quantity"]
+      ]);
+    }
+
+    http_response_code(200);
+    return [
+      "order_id" => $orders_info[0]->id,
+      "username" => $orders_info[0]->username,
+      "street" => $orders_info[0]->street,
+      "number" => $orders_info[0]->number,
+      "district" => $orders_info[0]->district,
+      "state" => $orders_info[0]->state,
+      "zip" => $orders_info[0]->zip,
+      "products" => $products
+    ];
+  }
 }
