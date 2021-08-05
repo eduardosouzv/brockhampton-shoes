8f733d5b350b9f1a733e8e5a500ef4fc6cc7ee63
Eduardo Souza <eduardosouzv@gmail.com>
2021-08-05 13:35:17 -0300

    fix: login validation

--- frontend/customer/js/login.js
+++ frontend/customer/js/login.js
@@ -5,33 +5,38 @@ async function authenticateUser() {
     e.preventDefault();
   });
 
-  const user = document.querySelector('#user').value;
-  const password = document.querySelector('#password').value;
+  const user = document.querySelector('#user');
+  const password = document.querySelector('#password');
+  user.style.border = 'none';
+  password.style.border = 'none';
 
-  if (!user || !password) {
+  if (!user.value || !password.value) {
+    user.style.border = '1px solid red';
+    password.style.border = '1px solid red';
+    return;
+  }
+  const response = await fetch(`${BASE_URL}/sessions/generateSession.php`, {
+    method: 'POST',
+    body: JSON.stringify({
+      user: user.value,
+      password: password.value,
+    }),
+  });
+  const { token, is_admin } = await response.json();
+
+  localStorage.setItem('token', token);
+  localStorage.setItem('cart', JSON.stringify([]));
+
+  if (is_admin) {
+    window.location.href = '/admin/products';
     return;
   }
 
-  try {
-    const response = await fetch(`${BASE_URL}/sessions/generateSession.php`, {
-      method: 'POST',
-      body: JSON.stringify({
-        user,
-        password,
-      }),
-    });
-    const { token, is_admin } = await response.json();
-
-    localStorage.setItem('token', token);
-    localStorage.setItem('cart', JSON.stringify([]));
-
-    if (is_admin) {
-      window.location.href = '/admin/products';
-      return;
-    }
-
-    window.location.href = '/shop';
-  } catch (error) {
-    console.log(error);
+  if (response.status === 401) {
+    user.style.border = '1px solid red';
+    password.style.border = '1px solid red';
+    return;
   }
+
+  location.href = '/shop';
 }
